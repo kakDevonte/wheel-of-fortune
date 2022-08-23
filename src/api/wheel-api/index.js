7828060e1c0b6fa3731cac564173e13a891a7d85
HatyushinAS <kakdevonte@yandex.ru>
2022-08-23 07:12:36 +0300

    add change

--- src/api/wheel-api/index.js
+++ src/api/wheel-api/index.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api/',
+  //baseURL: 'http://localhost:5000/api/',
+  baseURL: 'https://dead-pear-chipmunk-suit.cyclic.app/api/',
 });
 
 export const wheelAPI = {
